76e2f9ed96f9fa25ffd1b721862e232cf5bd169f
Howard Luna <HowardLuna@gmail.com>
2025-10-19 17:24:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2882775050-18569",
+  "appId": "1:301300120429:web:56f59894b97863e143f864",
+  "apiKey": "AIzaSyD-LjKp8-Qdj0ube1vfa6ju1MJ2SosOCu4",
+  "authDomain": "studio-2882775050-18569.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "301300120429"
+};
